e29a7fcd63e79b824f46045a6e24ff164ef28f5f
jaganmohan reddy <jaganre8179@gmail.com>
2022-04-29 07:59:43 +0530

    new changes made

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -14,13 +14,11 @@ const isValidRequestBody = function (requestBody) {
   return Object.keys({requestBody}).length > 0;
 };
 
-const isValidObjectId = function (objectId) {
-  return mongoose.Types.ObjectId.isValid(objectId);
-};
+
 
 const isValidString = function (value) {
   if (typeof value === "string" && value.trim().length === 0) return false;
   return true;
 };
 
-module.exports = { isValid,isValidTitle,isValidRequestBody,isValidObjectId,isValidString,}
+module.exports = { isValid,isValidTitle,isValidRequestBody,isValidString,}
